f8e355dd820843f06de77a0c2a91d267ba533d39
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-16 19:10:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,81 @@
-//complete this code
+// //complete this code
+// class Animal {
+// 	constructor(species){
+// 		this.spcies=species
+// 	}
+
+// 	get species(){
+// 		return this._species
+// 	}
+
+// 	makeSound(){
+// 	console.log(`The ${this.species} makes a sound`)
+// 	}
+// }
+
+// class Dog extends Animal {
+// 	constructor (species){
+// 		super(species)
+// 	}
+// 	bark(){
+// 		console.log("woof")
+// 	}
+// }
+
+// class Cat extends Animal {
+// 	constructor (species){
+// 		super(species)
+// 	}
+// 	purr(){
+// 		console.log("purr")
+// 	}
+// }
+
+// // Do not change the code below this line
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
+
 class Animal {
-	constructor(species){
-		this.spcies=species
-	}
+    constructor(species) {
+        this.species = species; // ✅ Corrected property name
+    }
 
-	get species(){
-		return this._species
-	}
+    get species() { // ✅ Fixed getter syntax
+        return this._species;
+    }
 
-	makeSound(){
-	console.log(`The ${this.species} makes a sound`)
-	}
+    set species(value) { // ✅ Added setter to store species properly
+        this._species = value;
+    }
+
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`); // ✅ Fixed string formatting
+    }
 }
 
 class Dog extends Animal {
-	constructor (species){
-		super(species)
-	}
-	bark(){
-		console.log("woof")
-	}
+    constructor(species) {
+        super(species);
+    }
+
+    bark() {
+        console.log("woof"); // ✅ Removed unnecessary `return`
+    }
 }
 
 class Cat extends Animal {
-	constructor (species){
-		super(species)
-	}
-	purr(){
-		console.log("purr")
-	}
+    constructor(species) {
+        super(species);
+    }
+
+    purr() {
+        console.log("purr"); // ✅ Removed unnecessary `return`
+    }
 }
 
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
